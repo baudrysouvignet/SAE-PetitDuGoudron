b56205bd161b0eeaaa2498d96a037c17b5effb6c
mathis oudin <mathis.oudin@etu.uca.fr>
2023-12-14 09:52:43 +0100

    files organisation + navbar html and css

---
+++ public/views/partials/nav.php
@@ -0,0 +1,12 @@
+<head>
+  <link rel="stylesheet" href="../../styles/partials/styles_nav.css">
+</head>
+
+<div class="nav">
+  <ul>
+    <li><a href="#"><img src="../img/nav/home.svg" alt="Accueil"></a></li>
+    <li><a href="#"><img src="../img/nav/forum.svg" alt="Forum"></a></li>
+    <li><a href="#"><img src="../img/nav/account.svg" alt="Compte"></a></li>
+    <li><a href="#" id="icon-logo"><img src="../img/nav/logo.svg" alt="Logo de la régie du quartier"></a></li>
+  </ul>
+</div>
