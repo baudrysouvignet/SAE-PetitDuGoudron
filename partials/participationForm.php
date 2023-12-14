6c69f6b98b6c77e3f7dbcba579c32c37443e912e
Baudry Souvignet <baudrysouvignet07@gmail.com>
2023-12-14 17:51:39 +0100

    🙋‍ Inclusion of the registration form, display within the account, but currently encountering an error during the update

---
+++ partials/participationForm.php
@@ -0,0 +1,55 @@
+<?php
+
+$isValue = isset($infoForm);
+?>
+
+<form action="<?= $field ?>" method="post">
+    <label for="firstName">Prénom de l'enfant :</label>
+    <input type="text" id="firstName" name="firstName" <?= $isValue ? 'value ='. $infoForm['firstName'] : ""?> required>
+    
+    <label for="date">Date de Naissance :</label>
+    <input type="date" id="date" name="date" <?= $isValue ? 'value ='. $infoForm['date'] : ""?> required>
+    
+    <label for="sexe">Sexe :</label>
+
+    <input type="radio" id="sexH" name="sexe" value="1" <?= $isValue && $infoForm['sexe'] == 1 ? 'checked="checked"' : ""?> checked="checked">
+    <label for="sexH">Masculin</label>
+
+    <input type="radio" id="sexF" name="sexe" value="0" <?= $isValue && $infoForm['sexe'] == 0 ? 'selected' : ""?>>
+    <label for="sexF">Féminin</label>
+    
+    <label for="adress">Adresse :</label>
+    <input type="text" id="adress" name="adresse" <?= $isValue ? 'value ='. $infoForm['adress'] : ""?> required>
+    
+    <label for="phone">Téléphone :</label>
+    <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" <?= $isValue ? 'value ='. $infoForm['phone'] : ""?> required>
+    
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" <?= $isValue ? 'value ='. $infoForm['email'] : ""?> required>
+    
+    <label for="allergy">Allergies :</label>
+    <textarea id="allergy" name="allergy" <?= $isValue ? 'value ='. $infoForm['allergy'] : ""?>></textarea>
+    
+    <label for="health">Conditions Médicales :</label>
+    <textarea id="health" name="health" <?= $isValue ? 'value ='. $infoForm['health'] : ""?>></textarea>
+    
+    <label for="drug">Médicaments :</label>
+    <textarea id="drug" name="drug"  <?= $isValue ? 'value ='. $infoForm['drug'] : ""?>></textarea>
+    
+    <label for="doctorPhone">Téléphone du Médecin :</label>
+    <input type="tel" id="doctorPhone" name="doctorPhone" pattern="[0-9]{10}"  <?= $isValue ? 'value ='. $infoForm['doctorPhone'] : ""?> required>
+    
+    <label for="secu">Numéro de Sécurité Sociale :</label>
+    <input type="text" id="secu" name="secu" <?= $isValue ? 'value ='. $infoForm['secu'] : ""?> required>
+    
+    <input type="hidden" name="id_utilisateur" value="<?= $user->loggedInUser[0]["ID_Utilisateur"] ?>">
+    <input type="hidden" name="id_form" value="<?= $isValue ? $infoForm['ID_Insription'] : ""?>">
+    
+    <label for="autorisation_parentale">Autorisation Parentale :</label>
+    <select id="autorisation_parentale" name="autorisation_parentale" required>
+        <option <?= $isValue ? 'selected' : ""?> value="1" >Oui</option>
+        <option value="0">Non</option>
+    </select>
+
+    <input type="submit" name="participer" value="<?= $btnName ?>">
+</form>
--- public/views/pages/participer.php
+++ public/views/pages/participer.php
@@ -1,1 +1,57 @@
 <?php
+session_start ();
+
+use entity\userEntity;
+
+require_once ('../../../config/DatabaseManager.php');
+require_once ('../../../entity/userEntity.php');
+require_once ('../../../controller/participationController.php');
+
+$database = DatabaseManager::getInstance();
+$user = new userEntity(
+    databaseManager: $database
+);
+if (isset($_SESSION['user_info'])){
+    $user->recoverUserData ($_SESSION['user_info']);
+} else {
+    header ("Location: connexion.php");
+    exit();
+}
+
+if (isset($_POST['participer'])) {
+    $data = [
+            "firstName" => $_POST['firstName'],
+            "date" => $_POST['date'],
+            "sexe" => $_POST['sexe'],
+            "adress" => $_POST['adresse'],
+            "phone" => (int)$_POST['phone'],
+            "email" => $_POST['email'],
+            "allergy" => $_POST['allergy'],
+            "health" => $_POST['health'],
+            "drug" => $_POST['drug'],
+            "doctorPhone" => (int)$_POST['doctorPhone'],
+            "secu" => (int)$_POST['secu'],
+            "autorisation_parentale" => $_POST['autorisation_parentale'],
+            "id_utilisateur" => $_POST['id_utilisateur']
+    ];
+
+    addParticipation(
+        databaseManager: $database,
+        data: $data
+    );
+
+
+    header ("Location: reglage.php");
+    exit();
+}
+
+$field = 'participer.php';
+$btnName = "S'inscrire";
+
+?>
+
+<h2>Formulaire d'inscription</h2>
+
+<?php
+include "../../../partials/participationForm.php";
+?>
