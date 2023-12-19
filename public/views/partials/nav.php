f0e9dacbcd0e0cb51ce8aca262d7fd1988a2f025
pihbe <pierrick.milazzo@gmail.com>
2023-12-19 14:06:35 +0100

    🔒 Fixed Header/nav urls

--- public/views/partials/nav.php
+++ public/views/partials/nav.php
@@ -1,4 +1,10 @@
-
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+$beforLink = '../';
+if ($current_page === 'index.php') {
+    $beforLink = 'views/';
+}
+?>
 
 <div class="container">
     <div class="navbar">
@@ -13,15 +19,15 @@
             <div class="col flex">
                 <div class="col">
                     <div class="nav-link">
-                        <a href="#"><img src="../img/nav/home.svg" alt="Accueil">Accueil</a>
+                        <a href="#"><img src="<?= $beforLink.'img/nav/home.svg'?>" alt="Accueil">Accueil</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                     <div class="nav-link">
-                        <a href="#"><img src="../img/nav/forum.svg" alt="Forum">Forum</a>
+                        <a href="#"><img src="<?= $beforLink.'img/nav/forum.svg'?>" alt="Forum">Forum</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                     <div class="nav-link">
-                        <a href="#"><img src="../img/nav/account.svg" alt="Compte">Compte</a>
+                        <a href="#"><img src="<?= $beforLink.'img/nav/account.svg'?>" alt="Compte">Compte</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                 </div>
