32408fa10cf233de6569bc93cd15775472061604
baudrysouvignet <baudrysouvignet07@gmail.com>
2023-12-20 18:12:49 +0100

    🗺️ Fixed Nav link

--- public/views/partials/nav.php
+++ public/views/partials/nav.php
@@ -1,8 +1,10 @@
 <?php
 $current_page = basename($_SERVER['PHP_SELF']);
 $beforLink = '../';
+$beforLinkNav = '';
 if ($current_page === 'index.php') {
     $beforLink = 'views/';
+    $beforLinkNav = 'views/pages/';
 }
 ?>
 
@@ -19,19 +21,19 @@ if ($current_page === 'index.php') {
             <div class="col flex">
                 <div class="col">
                     <div class="nav-link">
-                        <a href="#"><img src="<?= $beforLink.'img/nav/home.svg'?>" alt="Accueil">Accueil</a>
+                        <a href="<?= $current_page === 'index.php'? '#' : '../../index.php'?>"><img src="<?= $beforLink.'img/nav/home.svg'?>" alt="Accueil">Accueil</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                     <div class="nav-link">
-                        <a href="#"><img src="<?= $beforLink.'img/nav/forum.svg'?>" alt="Forum">Forum</a>
+                        <a href="<?= $beforLinkNav.'forum.php' ?>"><img src="<?= $beforLink.'img/nav/forum.svg'?>" alt="Forum">Forum</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                     <div class="nav-link">
-                        <a href="#"><img src="<?= $beforLink.'img/nav/account.svg'?>" alt="Compte">Compte</a>
+                        <a href="<?= $beforLinkNav.'connexion.php'?>"><img src="<?= $beforLink.'img/nav/account.svg'?>" alt="Compte">Compte</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                     <div class="nav-link">
-                        <a href="#"><img src="<?= $beforLink.'img/nav/account.svg'?>" alt="Compte">Participer</a>
+                        <a href="<?= $beforLinkNav.'participer.php'?>"><img src="<?= $beforLink.'img/nav/account.svg'?>" alt="Perticiper">Participer</a>
                         <div class="nav-item-wrapper"></div>
                     </div>
                 </div>
